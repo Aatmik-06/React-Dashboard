a15829cbbf6158647993912a5bf067e4d48cf93c
Aatmik-06 <adi120kush@gmail.com>
2024-10-19 23:25:20 +0530

    home page updated

--- React Dashboard/src/Components/Home.jsx
+++ React Dashboard/src/Components/Home.jsx
@@ -1,23 +1,54 @@
-import Container from 'react-bootstrap/Container';
-import user from "../images/Users.png"
-import products from "../images/Products.png"
-import orders from "../images/Orders.png"
-import review from "../images/Reviews.png"
-import sales from "../images/Sales.png"
-const HomeProducts=()=>{
-    return(
-        <>
-        <Container id='homecontainer'>
-            <div className='homecontent'>
-                <div id='homecontent-h1'><img src={user} alt="" /> <br />  <br /><img src={orders} alt="" /></div>
-                <div id='homecontent-h3'> <img src={sales} alt="" /> </div>
+import Container from "react-bootstrap/Container";
+import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faArrowTrendUp } from "@fortawesome/free-solid-svg-icons";
+const HomeProducts = () => {
+  return (
+    <>
+      <Container id="homecontainer">
+        <div className="homecontent">
+
+          <div id="homecontent-h1">
+            <div id="users">
+                <p>Total Users</p> 
+                <h1>510</h1> 
+                <br />
+                <p><span>+95%</span> Last Month</p>
             </div>
-            <div id='homecontent-h2'><img src={products} alt="" /> <br /> <br /> <img src={review} alt="" id='review' /></div>
-                
-        </Container>
-        
-        </>
-    )
-}
+            <div id="orders">
+                <p>Total Orders</p> 
+                <h1>482</h1> 
+                <br />
+                <p><span>+30%</span> Last Month</p>
+            </div>       
+          </div>
+
+          <div id="homecontent-h2">
+            <div>
+                <h3>Total Sales</h3> <br />
+                <h1>$3,787,681.00 <span>40.63% <FontAwesomeIcon icon={faArrowTrendUp} /> </span></h1> <br /><br /> <br /><br /> <br /><br /><br />
+                <p>$3,578.90 in last month</p>
+            </div>
+          </div>
+        </div>
+
+        <div id="homecontent-h3">
+            <div id="products">
+                    <p>Total Products</p> 
+                    <h1>320</h1> 
+                    <br />
+                    <p><span>+25%</span> Last Month</p>
+            </div> <br /> <br />
+            <div id="reviews">
+                <p>Total Reviews</p> 
+                <h1>159</h1> 
+                <br />
+                <p><span>+45%</span> Last Month</p>
+            </div>
+          
+        </div>
+      </Container>
+    </>
+  );
+};
 
 export default HomeProducts;
