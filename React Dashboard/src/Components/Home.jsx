d8e8e873be5ffcf15c9c3613b89e692164266945
Aatmik-06 <adi120kush@gmail.com>
2024-10-19 18:45:15 +0530

    home page updated

--- React Dashboard/src/Components/Home.jsx
+++ React Dashboard/src/Components/Home.jsx
@@ -1,7 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import user from "../images/Users.png"
+import products from "../images/Products.png"
+import orders from "../images/Orders.png"
+import review from "../images/Reviews.png"
+import sales from "../images/Sales.png"
 const HomeProducts=()=>{
     return(
         <>
-        <h1>Home Products</h1>
+        <Container id='homecontainer'>
+            <div className='homecontent'>
+                <div id='homecontent-h1'><img src={user} alt="" /> <br />  <br /><img src={orders} alt="" /></div>
+                <div id='homecontent-h3'> <img src={sales} alt="" /> </div>
+            </div>
+            <div id='homecontent-h2'><img src={products} alt="" /> <br /> <br /> <img src={review} alt="" /></div>
+                
+        </Container>
+        
         </>
     )
 }
--- React Dashboard/src/Components/Layout.jsx
+++ React Dashboard/src/Components/Layout.jsx
@@ -3,19 +3,30 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import logo from "../images/Main-logo.png";
+import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faBookOpen } from "@fortawesome/free-solid-svg-icons";
+import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faFilePen } from "@fortawesome/free-solid-svg-icons";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 const Layout = () => {
   return (
     <>
       <Navbar data-bs-theme="dark" id="navbar">
-     <Navbar.Brand as={Link} to="./home">
-          <img src={logo} alt="logo" style={{height:"60px"}}/>
-          Products Dashboard
+     <Navbar.Brand as={Link} to="./home" id="logo">
+     <div>
+        <img src={logo} alt="logo" style={{height:"60px"}}/>
+     </div>
+         <div>
+         <h2>Products <br /> <h5> Dashboard</h5> </h2>
+         </div>
         </Navbar.Brand>     
         <Container className="navbar">
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
             <Navbar.Text>
-              Signed in as: <a href="#login">Aatmik Kushwaha</a>
+              Signed in as : <a href="#login">Aatmik Kushwaha</a>
             </Navbar.Text>
           </Navbar.Collapse>
         </Container>
@@ -25,25 +36,27 @@ const Layout = () => {
       <aside>
         <Nav defaultActiveKey="/home" className="flex-column" id="sidebar">
           <Nav.Link id="sideitems" as={Link} to="/home">
-            Dashboard
+          <FontAwesomeIcon icon={faHouse} /> Dashboard
           </Nav.Link>
           <Nav.Link id="sideitems" as={Link} to="/viewproducts">
-            View Products
+          <FontAwesomeIcon icon={faBookOpen} /> View Products
           </Nav.Link>
           <Nav.Link id="sideitems" as={Link} to="/addproducts">
-            Add Products
+          <FontAwesomeIcon icon={faBriefcase} /> Upload Products
           </Nav.Link>
           <Nav.Link id="sideitems" as={Link} to="/searchproducts">
-            Search Products
+          <FontAwesomeIcon icon={faMagnifyingGlass} />  Search Products
           </Nav.Link>
           <Nav.Link id="sideitems" as={Link} to="/updateproducts">
-            Update Products
+          <FontAwesomeIcon icon={faFilePen} />  Update Products
           </Nav.Link>
-          <Nav.Link id="sideitems" eventKey="disabled" disabled>
-            Logout
+          <Nav.Link id="sideitems" as={Link} to="/login">
+          <FontAwesomeIcon icon={faRightFromBracket} /> Logout
           </Nav.Link>
         </Nav>
+        <div id="outlet">
         <Outlet/>
+        </div>
       </aside>
       
     </>
